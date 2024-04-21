60002d3749267518e19d18184f33835344936a7a
sutneV <brofist25ventus@gmail.com>
2024-04-21 23:41:28 +0800

    Updates

---
+++ precache-manifest.059945add35eb9561ed1d66db614090d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c6399530bf78e1674034d0f7b592843",
+    "url": "/best-website-everrr/index.html"
+  },
+  {
+    "revision": "9e208529ffd47898cac0",
+    "url": "/best-website-everrr/static/css/main.a84c8a54.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/best-website-everrr/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/best-website-everrr/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "9e208529ffd47898cac0",
+    "url": "/best-website-everrr/static/js/main.602c987d.chunk.js"
+  },
+  {
+    "revision": "80d951c80790f565b50e",
+    "url": "/best-website-everrr/static/js/runtime~main.173a276d.js"
+  }
+]);
